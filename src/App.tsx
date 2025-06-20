b45dec33d1bd0e26f30c541015ff0b6100d920c0
tohara007 <tohara007@example.com>
2025-06-21 03:16:02 +0900

    TailwindCSS + DaisyUIの適用

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,7 +18,10 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button
+          className="btn btn-primary"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
         </button>
         <p>
@@ -28,8 +31,14 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <button
+        className="btn btn-secondary"
+        onClick={() => setCount((count) => count - 1)}
+      >
+        count is {count}
+      </button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
